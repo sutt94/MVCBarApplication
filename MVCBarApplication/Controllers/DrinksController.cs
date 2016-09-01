fd95f17e90f762e2f4bdf94446492bd2b8f9794b
sutt94 <sutt94@gmail.com>
2016-09-01 14:42:51 -0400

    update

--- MVCBarApplication/Controllers/DrinksController.cs
+++ MVCBarApplication/Controllers/DrinksController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCBarApplication.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace MVCBarApplication.Controllers
 {
@@ -15,6 +18,44 @@ namespace MVCBarApplication.Controllers
             DrinksContext drinksContext = new DrinksContext();
             Drinks[] drinks = drinksContext.Drinks.ToArray();
             return View(drinks);
+        }
+
+        public ActionResult Order(int id, string drinkName)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["DrinksContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddOrder", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramID = new SqlParameter();
+                paramID.ParameterName = "@DrinkID";
+                paramID.Value = id;
+                cmd.Parameters.Add(paramID);
+
+                SqlParameter paramDrinkName = new SqlParameter();
+                paramDrinkName.ParameterName = "@DrinkName";
+                paramDrinkName.Value = drinkName;
+                cmd.Parameters.Add(paramDrinkName);
+
+                SqlParameter paramOrderDate = new SqlParameter();
+                paramOrderDate.ParameterName = "@OrderDate";
+                paramOrderDate.Value = DateTime.Now;
+                cmd.Parameters.Add(paramOrderDate);
+
+                SqlParameter paramCustomerName = new SqlParameter();
+                paramCustomerName.ParameterName = "@CustomerName";
+                paramCustomerName.Value = "Test";
+                cmd.Parameters.Add(paramCustomerName);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+
+                return View();
+            }
         }
     }
 }
